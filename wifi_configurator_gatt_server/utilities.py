cbe5349c12a95d235a6755163813f7ce4ca8f11b
Daniel Couturier <dancout@umich.edu>
2022-01-05 07:51:26 -0500

    Created connected_to_internet() func

--- wifi_configurator_gatt_server/utilities.py
+++ wifi_configurator_gatt_server/utilities.py
@@ -44,3 +44,29 @@ network={{
 
     # After editing, we should change the file back to read-only
     os.system(f"sudo chmod 444 {wpa_file_path}")
+
+
+def connected_to_internet():
+    """
+    This function will return true or false whether there is currently
+    an internet connection or not.
+    """
+
+    # This represents the terminal command that will ping google.com to seek
+    # a response
+    ping = "ping -c2 google.com"
+
+    # Store output of the ping
+    output = os.popen(ping).read()
+
+    # Define a String that should only be present if connected to the internet
+    ping_success = "--- google.com ping statistics ---"
+
+    # Check if the success String is present in the output
+    if ping_success in output:
+        print("Connected to internet.")
+        return True
+    # Else, there is no internet connection
+    else:
+        print("Not connected to internet.")
+        return False
