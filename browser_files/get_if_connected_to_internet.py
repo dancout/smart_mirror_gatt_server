037b25cc4421f661db5dac45f8a81f8552dc88db
Daniel Couturier <dancout@umich.edu>
2022-03-13 21:13:08 -0400

    Proper imports for function reference

--- browser_files/get_if_connected_to_internet.py
+++ browser_files/get_if_connected_to_internet.py
@@ -1,3 +1,7 @@
-from utilities import connected_to_internet
+import sys
+# The below will allow this file to import functions from parallel directories
+sys.path.insert(0, '/home/pi/Documents/Projects/smart_mirror_gatt_server')
+
+from wifi_configurator_gatt_server.utilities import connected_to_internet
 
 print(connected_to_internet())
