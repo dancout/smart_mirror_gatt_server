d382c9dce9cbc4c06dfc7ab68ef49693c4b3dd6d
Daniel Couturier <dancout@umich.edu>
2022-03-24 01:23:12 +0000

    Added script to run all installations in one go plus error checking

---
+++ scripts/fresh_install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Take an input of what the device name will be and save as a variable
+
+echo "Enter the DISPLAY_DEVICE_NAME specified from the Firebase Document:"
+
+read DISPLAY_DEVICE_NAME
+
+# Enter Documents
+cd /home/pi/Documents
+
+# create the Projects directory if it does not exist
+PROJ="/home/pi/Documents/Projects/"
+if [ ! -d "$PROJ" ]; then
+  echo "Creating Projects directory"
+  mkdir Projects
+fi
+
+# Enter Projects
+cd Projects
+
+# clone the project
+git clone https://github.com/dancout/smart_mirror_gatt_server.git
+
+# Check for any errors
+if [[ $? > 0 ]]
+then
+    echo "Cloning git repo failed. Exiting."
+    return 1
+else
+    echo "Successfully cloned repo"
+fi
+
+# Enter smart_mirror_gatt_server/scripts
+cd smart_mirror_gatt_server/scripts
+
+# run install dependencies
+. install_dependencies.sh 
+
+# Check for any errors
+if [[ $? > 0 ]]
+then
+    echo "Installing dependencies failed. Exiting"
+    return 1
+else
+    echo "Successfully installed dependencies"
+fi
+
+# run set display device name with input device variable
+. set_display_device_name.sh $DISPLAY_DEVICE_NAME
+
+# Check for any errors
+if [[ $? > 0 ]]
+then
+    echo "Setting up device name failed. Exiting"
+    return 1
+else
+    echo "Successfully set up device name"
+fi
+
+# Message user that setup has completed
+echo "Setup complete!"
+echo "Sleeping for 5 seconds before rebooting"
+
+# Sleep for 5 seconds for the user to read the message
+sleep 5
+
+# finally, reboot the machine so that the changes take effect
+sudo reboot
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,12 +4,39 @@
 sudo apt-get update -y
 sudo apt-get dist-upgrade -y
 
+# Check for any errors
+if [[ $? > 0 ]]
+then
+    echo "Updating packages failed. Exiting"
+    return 1
+else
+    echo "Successfully updated packages"
+fi
+
 # Install UPower to avoid multiple errors when opening the Chrome Browser
 # from the command line.
 sudo apt-get install -y upower
 
+# Check for any errors
+if [[ $? > 0 ]]
+then
+    echo "Installing upower failed. Exiting"
+    return 1
+else
+    echo "Successfully installed upower"
+fi
+
 # Install unclutter to hide the mouse cursor
 sudo apt-get install -y unclutter
+
+# Check for any errors
+if [[ $? > 0 ]]
+then
+    echo "Installing unclutter failed. Exiting"
+    return 1
+else
+    echo "Successfully installed unclutter"
+fi
 
 # Update the autostart file so that the display never goes to sleep
 # Make the autostart file editable
