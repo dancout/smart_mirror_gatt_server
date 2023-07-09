0d54eb12eb64bbc23b27984a609851c42025fdb9
Daniel Couturier <dancout@umich.edu>
2023-07-09 19:27:23 -0400

    Updated elif statements

--- scripts/set_screen_rotation.sh
+++ scripts/set_screen_rotation.sh
@@ -11,26 +11,23 @@ SCREEN_ROTATION=${SCREEN_ROTATION^^}
 
 CONVERTED_ROTATION_VALUE=0
 
-if [[ "$SCREEN_ROTATION" == "NORMAL" ]]
-then
-    CONVERTED_ROTATION_VALUE=0
 
+
+if [[  "$SCREEN_ROTATION" == "NORMAL" ]]
+then
+     CONVERTED_ROTATION_VALUE=0
 elif [[ "$SCREEN_ROTATION" == "LEFT" ]]
 then
-    CONVERTED_ROTATION_VALUE=1
-
-elif [[ "$SCREEN_ROTATION" == "UPSIDE_DOWN"]]
+     CONVERTED_ROTATION_VALUE=1
+elif [[ "$SCREEN_ROTATION" == "UPSIDE_DOWN" ]]
 then
-    CONVERTED_ROTATION_VALUE=2
-
-elif [[ "$SCREEN_ROTATION" == "RIGHT"]]
+     CONVERTED_ROTATION_VALUE=2
+elif [[ "$SCREEN_ROTATION" == "RIGHT" ]]
 then
-    CONVERTED_ROTATION_VALUE=3
-
+     CONVERTED_ROTATION_VALUE=3  
 else
-then 
-    echo "Please enter one of the following values: NORMAL, LEFT, RIGHT, UPSIDE_DOWN"
-    return 1
+     echo "Please enter one of the following values: NORMAL, LEFT, RIGHT, UPSIDE_DOWN"
+     return 1
 fi
 
 echo "xrandr -o {{CONVERTED_ROTATION_VALUE}} > "rotate_screen.sh"
